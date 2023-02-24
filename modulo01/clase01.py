b73e66fde2d256486f1f9bb03df38e6f0beda0e5
Manuel García <manoloache@gmail.com>
2023-02-23 21:23:11 -0500

    Funciones

--- modulo01/clase01.py
+++ modulo01/clase01.py
@@ -1,4 +1,18 @@
-print("Hola, como estan esta es mi primera aplicacion")
+# print("Hola, como estan esta es mi primera aplicacion")
 
-print("Probando Hithub")
+# print("Probando Hithub")
 
+def sumar(num01, num02):
+    total = num01 + num02
+    print(f"El resultado de la suma es: {total}")
+
+sumar(5, 14)
+
+print("===================================")
+
+def multiplicar(num01, num02):
+    total = num01 * num02
+    return total
+
+val_multi = multiplicar(5,10)
+print(f"El resultado de la suma es: {val_multi}")
