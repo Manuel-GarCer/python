603b1835623d3c581146e697f3f1d0394deba96e
Manuel García <manoloache@gmail.com>
2023-04-27 00:04:35 -0500

    Ultima clase y repaso

--- modulo04/sistema_colegio/app/academico/admin.py
+++ modulo04/sistema_colegio/app/academico/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Estudiante
+from .models import Estudiante, Curso, Nota
 
 # Register your models here.
 
@@ -12,3 +12,18 @@ class EstudianteAdmin(admin.ModelAdmin):
     # exclude = ["genero"]
     # ordering = ["id"]
     # search_fields = ['nombre', 'apellido']
+
+@admin.register(Curso)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'descripcion']
+
+
+@admin.register(Nota)
+class NotaAdmin(admin.ModelAdmin):
+    list_display = ['id','nombre_estudiante', 'nombre_curso']
+
+    def nombre_estudiante(self, obj):
+        return obj.estudiante.nombre
+    
+    def nombre_curso(self, obj):
+        return obj.curso.nombre
--- modulo04/sistema_colegio/app/academico/models.py
+++ modulo04/sistema_colegio/app/academico/models.py
@@ -9,4 +9,27 @@ class Estudiante (models.Model):
     )
     nombre  = models.CharField(max_length=50) 
     apellido  = models.CharField(max_length=50) 
-    genero  = models.CharField(max_length=1, choices=CHOISE_GENERO) 
+    genero  = models.CharField(max_length=1, choices=CHOISE_GENERO)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}" # __unicode__ on Python 2
+
+
+class Curso (models.Model):
+    nombre  = models.CharField(max_length=50)
+    descripcion  = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.nombre}" 
+
+
+class Nota (models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota01 = models.DecimalField(max_digits=5, decimal_places=2)
+    nota02 = models.DecimalField(max_digits=5, decimal_places=2)
+    nota03 = models.DecimalField(max_digits=5, decimal_places=2)
+    nota04 = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = (('estudiante', 'curso'), )
